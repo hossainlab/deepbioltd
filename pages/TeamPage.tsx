99129370da0e9a171a78a6373c6e4bf6faf6167d
Jubayer Hossain <work.jubayer@gmail.com>
2025-07-25 02:45:27 +0600

    fix relative image path

--- pages/TeamPage.tsx
+++ pages/TeamPage.tsx
@@ -42,7 +42,7 @@ const leadership = [
     name: 'Md. Jubayer Hossain',
     role: 'Founder & Chief Executive Officer',
     desc: 'Mr. Jubayer is a visionary leader with extensive experience in bioinformatics and AI strategy.',
-    img: 'img/team/jubayer-ceo.jpg',
+    img: './img/team/jubayer-ceo.jpg',
     color: 'border-primary',
     socials: {
       linkedin: 'https://linkedin.com/in/jubayer',
